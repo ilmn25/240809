eb38b0d491dbdb47a5a79906a8bbbc73c7a73b81
ilmn25 <kilmn025@gmail.com>
2024-11-25 20:43:07 +0800

    0.0.34 made rarity enum

--- Assets/Script/Container/ItemData.cs
+++ Assets/Script/Container/ItemData.cs
@@ -1,17 +1,19 @@
- 
+
+
+public enum ItemRarity{ Common, Rare, Epic, Legendary }
 public class ItemData
 {
     public string StringID { get; set; }
     public string Name { get; set; }
     public int StackSize { get; set; }
-    public string Rarity { get; set; }
+    public ItemRarity Rarity { get; set; }
     public int Damage { get; set; }
     public float Knockback { get; set; }
     public int UseTime { get; set; }
     public string Description { get; set; }
     public bool IsConsumable { get; set; }
 
-    public ItemData(string stringID, string name, int stackSize, string rarity, string description, bool isConsumable, int damage, float knockback, int useTime )
+    public ItemData(string stringID, string name, int stackSize, ItemRarity rarity, string description, bool isConsumable, int damage, float knockback, int useTime )
     {
         StringID = stringID;
         Name = name;
