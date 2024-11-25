f4c34ca327b3e57638d6803d381d4c30a45917d2
ilmn25 <kilmn025@gmail.com>
2024-11-25 15:24:26 +0800

    0.0.29 item physics fix (basic)

--- Assets/Script/World/Entity/Item/ItemPhysicInst.cs
+++ Assets/Script/World/Entity/Item/ItemPhysicInst.cs
@@ -8,11 +8,11 @@ public class ItemPhysicInst : MonoBehaviour
     private LayerMask _collisionLayer;
     private Vector3 _velocity;
     private float _deltaTime;
-    private float GRAVITY = 50;  
-    private float ESCAPE_VELOCITY = 20;  
-    private float BOUNCE_THRESHOLD = 0.5f; 
-    private float BOUNCE_FACTOR = 0.4f;  
-    private float SLIDE_RANGE = 1;  
+    private float GRAVITY = 35;  
+    private float ESCAPE_VELOCITY = 5;  
+    private float BOUNCE_THRESHOLD = 5f; 
+    private float BOUNCE_FACTOR = 0.3f;  
+    private float SLIDE_RANGE = 3;  
     private float PICKUP_RANGE = 0.8f;
     private float COLLISION_RANGE = 0.3f;  
 
@@ -20,13 +20,13 @@ public class ItemPhysicInst : MonoBehaviour
     {
         Instance = this;
         _collisionLayer = LayerMask.GetMask("Collision"); 
-        transform.position = transform.position + new Vector3(0, 0.1f, 0);
+        transform.position += new Vector3(0, 0.1f, 0);
         PopItem();
     }
 
     public void PopItem() //initial pop velocity 
     {
-        _velocity = new Vector3(Random.Range(-SLIDE_RANGE, SLIDE_RANGE), 0.1f, Random.Range(-SLIDE_RANGE, SLIDE_RANGE));  
+        _velocity = new Vector3(Random.Range(-SLIDE_RANGE, SLIDE_RANGE), 5f, Random.Range(-SLIDE_RANGE, SLIDE_RANGE));  
     }
 
     void Update()
@@ -51,34 +51,55 @@ public class ItemPhysicInst : MonoBehaviour
 
     void HandlePhysics()
     {
-        _deltaTime = GameStatic._deltaTime;
+        _deltaTime = GameStatic.GetDeltaTime();
 
-        // Check if the object is inside a block
-        if (Physics.CheckSphere(transform.position, COLLISION_RANGE/1.5f, _collisionLayer))
-        {   
-            _velocity = Vector3.up * ESCAPE_VELOCITY;
-            transform.position = transform.position + _velocity * _deltaTime; 
+        if (!IsMovable(transform.position))
+        {
+            transform.position += new Vector3(0, 5, 0) * _deltaTime;
             return;
         }
 
-        _velocity += Vector3.down * GRAVITY * _deltaTime;
+        _velocity += GRAVITY * _deltaTime * Vector3.down;
         _velocity.y = Mathf.Max(_velocity.y, -GRAVITY); 
         Vector3 newPosition = transform.position + _velocity * _deltaTime;
-        RaycastHit hit;
-        if (Physics.SphereCast(transform.position, COLLISION_RANGE, _velocity.normalized, out hit, _velocity.magnitude * Time.fixedDeltaTime, _collisionLayer))
-        { 
-            if (Mathf.Abs(_velocity.y) < BOUNCE_THRESHOLD)
-            {
-                _velocity = Vector3.Reflect(_velocity, hit.normal) * BOUNCE_FACTOR; 
-            } else 
-            {
-                _velocity = Vector3.zero;
-            }
-        }
-        else
+        
+        if (IsMovable(newPosition))
         {
             transform.position = newPosition;
-        } 
+        }
+        else
+        { 
+            _velocity = -_velocity * BOUNCE_FACTOR;
+        }
+        // RaycastHit hit;
+        // if (Physics.SphereCast(transform.position, COLLISION_RANGE, _velocity.normalized, out hit, _velocity.magnitude * _deltaTime, _collisionLayer))
+        // {
+        //     if (Mathf.Abs(_velocity.y) > BOUNCE_THRESHOLD)
+        //     {
+        //         _velocity = Vector3.Reflect(_velocity, hit.normal) * BOUNCE_FACTOR; 
+        //     } else 
+        //     {
+        //         _velocity = Vector3.zero;
+        //     }
+        // }
+        // else
+        // {
+        //     if (IsMovable(newPosition))
+        //     {
+        //         transform.position = newPosition;
+        //     } 
+        // } 
+    }
+    
+    Collider[] tempCollisionArray = new Collider[1];
+    int collisionCount; 
+    private bool IsMovable(Vector3 newPosition)
+    {  
+        // Define an array to store the results
+        tempCollisionArray[0] = null;
+        collisionCount = Physics.OverlapSphereNonAlloc(newPosition + new Vector3(0,0.2f,0), COLLISION_RANGE, tempCollisionArray, _collisionLayer);
+
+        return !(collisionCount > 0);
     }
     
     //! DEBUG TOOLS
