cdb85448f925fdafeca5b52c7cb68016b4abb472
ilmn25 <kilmn025@gmail.com>
2025-08-05 21:27:40 +0800

    0.0.118 loottable system

--- Assets/Script/Game/Brain/State/TreeState.cs
+++ Assets/Script/Game/Brain/State/TreeState.cs
@@ -1,46 +1,46 @@
- 
-using System;
-using Unity.VisualScripting;
-using UnityEngine; 
- 
-
-public class TreeState : State
-{ 
-    public Transform _spriteObject;
-    private int _health;
-    private string _item;
-    private int _currentHealth;
-    public override void OnInitialize()
-    {
-        ((OakMachine)Machine).Hit += Hit;
-    }
-    public override void OnExitState()
-    {
-        ((OakMachine)Machine).Hit -= Hit;
-    }
-
-
-    public override void OnEnterState()
-    { 
-        _currentHealth = _health;
-        _spriteObject = Machine.transform.Find("sprite");
-        AddState(new ResourceCollapse(_spriteObject, _item));
-        AddState(new StaticIdle(), true);
-    }
-
-    public TreeState(string item, int health)
-    {
-        _item = item;
-        _health = health;
-    }
-
-    public void Hit()
-    {
-        Audio.PlaySFX("dig_grass");
-        if (GUI.Active) return;
-        _currentHealth--;
-        if (_currentHealth != 0) return;
-        SetState<ResourceCollapse>();
-        _currentHealth = _health;
-    }  
-}
+//  
+// using System;
+// using Unity.VisualScripting;
+// using UnityEngine; 
+//  
+//
+// public class TreeState : State
+// { 
+//     public Transform _spriteObject;
+//     private int _health;
+//     private string _item;
+//     private int _currentHealth;
+//     public override void OnInitialize()
+//     {
+//         ((OakMachine)Machine).Hit += Hit;
+//     }
+//     public override void OnExitState()
+//     {
+//         ((OakMachine)Machine).Hit -= Hit;
+//     }
+//
+//
+//     public override void OnEnterState()
+//     { 
+//         _currentHealth = _health;
+//         _spriteObject = Machine.transform.Find("sprite");
+//         AddState(new ResourceCollapse(_spriteObject, _item));
+//         AddState(new StaticIdle(), true);
+//     }
+//
+//     public TreeState(string item, int health)
+//     {
+//         _item = item;
+//         _health = health;
+//     }
+//
+//     public void Hit()
+//     {
+//         Audio.PlaySFX("dig_grass");
+//         if (GUI.Active) return;
+//         _currentHealth--;
+//         if (_currentHealth != 0) return;
+//         SetState<ResourceCollapse>();
+//         _currentHealth = _health;
+//     }  
+// }
---
+++ Assets/Script/Game/Item/Loot.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class Loot
+{ 
+    private static readonly Dictionary<string, Loot> Dictionary = new Dictionary<string, Loot>();
+    public static void Initialize()
+    {
+        Loot loot = CreateTable("slab");
+        loot.Add(1, "stone");
+        loot.Add(1, "stone");
+        loot.Add(1, "stone");
+        loot.Add(0.5f, "stone");
+        loot.Add(0.5f, "marble");
+        loot.Add(0.5f, "marble", "sand"); 
+        
+        loot = CreateTable("tree");
+        loot.Add(1, "wood");
+        loot.Add(1, "wood");
+        loot.Add(1, "wood");
+        loot.Add(0.5f, "wood");
+        loot.Add(0.5f, "wood");
+    }
+
+    public static Loot CreateTable(string id)
+    {
+        Loot loot = new Loot();
+        Dictionary.Add(id, loot);
+        return loot;
+    }
+    
+    public static Loot Gettable(string id)
+    {
+        return Dictionary[id];
+    }
+    
+    public readonly Dictionary<float, List<string>> Table = new Dictionary<float, List<string>>();
+    public void Add(float chance, params string[] itemIDs)
+    {
+        if (!Table.ContainsKey(chance))
+        {
+            Table[chance] = new List<string>();
+        }
+        Table[chance].AddRange(itemIDs);
+    }
+
+    public void Spawn(Vector3 position)
+    {
+        Vector3Int worldPosition = Vector3Int.FloorToInt(position);
+
+        foreach (var entry in Table)
+        {
+            float chance = entry.Key;
+            List<string> items = entry.Value;
+
+            if (Random.value <= chance && items.Count > 0)
+            {
+                string itemID = items[Random.Range(0, items.Count)];
+                Entity.SpawnItem(itemID, worldPosition);
+            }
+        }
+    }
+ 
+}
