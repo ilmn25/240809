33940aa3204ccd1a80ce7bd371834b78327391c0
ilmn25 <kilmn025@gmail.com>
2025-09-01 13:30:33 +0800

    0.0.197 BedMachine.cs fade to black coroutine instead

--- Assets/Script/Game/Brain/Machine/BedMachine.cs
+++ Assets/Script/Game/Brain/Machine/BedMachine.cs
@@ -3,29 +3,26 @@ using UnityEngine;
 
 public class BedMachine: StructureMachine, IActionSecondaryInteract
 {
-    private (int, int) _target = (-1, -1);
     public static Info CreateInfo()
     {
         return new Info();
     }
-    public void OnActionSecondary(Info info)
-    { 
-        if (_target.Item1 != -1) return; 
-        Game.PlayerInfo.spawnPoint = transform.position;
-        Game.PlayerInfo.SpeedModifier = 0.001f;
-        _target = Environment.CalculateTime(Environment.Length / 2);
-        Environment.TickSpeed = 10;
-    }
 
-    public override void OnUpdate()
+    public void OnActionSecondary(Info info)
     {
-        if (_target.Item1 == -1) return;
-        if (_target.Item1 <= World.Inst.day && _target.Item2 <= World.Inst.time)
-        {  
-            Environment.Target = EnvironmentType.Null;
-            _target.Item1 = -1;
-            Environment.TickSpeed = 1;
+        if (World.Inst.weather == EnvironmentType.Day) return;
+        _ = new CoroutineTask(Sleep());
+        return;
+
+        IEnumerator Sleep()
+        {
+            Environment.Target = EnvironmentType.Black;
+            Game.PlayerInfo.SpeedModifier = 0.001f;
+            yield return new WaitForSeconds(3);
+            Environment.MoveTime(Environment.Length / 2);
+            yield return new WaitForSeconds(3);
             Game.PlayerInfo.SpeedModifier = 1;
+            Environment.Target = EnvironmentType.Null;
         }
     }
 }
@@ -40,17 +37,17 @@ public class PortalMachine: StructureMachine, IActionSecondaryInteract
     public void OnActionSecondary(Info info)
     {
         _ = new CoroutineTask(Portal());
-    }
-
-    private IEnumerator Portal()
-    {
-        Environment.Target = EnvironmentType.Black; 
-        Game.PlayerInfo.SpeedModifier = 0.001f;
-        yield return new WaitForSeconds(2);
-        Game.Player.transform.position = new Vector3Int(2, 50, 2); 
-        yield return new WaitForSeconds(3);
-        Game.PlayerInfo.SpeedModifier = 1;
-        Environment.Target = EnvironmentType.Null; 
+        return;
+        IEnumerator Portal()
+        {
+            Environment.Target = EnvironmentType.Black; 
+            Game.PlayerInfo.SpeedModifier = 0.001f;
+            yield return new WaitForSeconds(2);
+            Game.Player.transform.position = new Vector3Int(2, 50, 2); 
+            yield return new WaitForSeconds(3);
+            Game.PlayerInfo.SpeedModifier = 1;
+            Environment.Target = EnvironmentType.Null; 
+        }
     }
 }
 public class SignMachine: StructureMachine, IActionSecondaryInteract
