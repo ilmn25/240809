b2870b085365d5045e2ac41a4e01f018314c781e
illu <kilmn025@gmail.com>
2026-02-22 00:02:31 +0800

    Added missed unversioned assets, Added particle system from footprint scripts

--- Assets/Script/Game/Particle.cs
+++ Assets/Script/Game/Particle.cs
@@ -1,20 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+public enum Particles {Smoke, Dust}
 public class Particle
 {
-    private static int _gibsCount = 0;
-    public static readonly int MaxGibs = 100;
-    
-    public static void Spawn(ID id, Vector3 position, Quaternion rotation, bool force = false)
+    private static readonly Queue<GameObject> Pool = new ();
+    private const int PoolSize = 20;
+    private static int _count = 0;
+    private static GameObject _particle;
+
+    static Particle()
     {
-        if (force || _gibsCount < MaxGibs)
+        GameObject prefab = Resources.Load<GameObject>("Prefab/Particle");
+        for (int i = 0; i < PoolSize; i++)
         {
-            GameObject obj = ObjectPool.GetObject(id);
-            obj.SetActive(true);
-            Debug.Log(rotation);
-            obj.transform.position = position;
-            obj.transform.rotation = rotation;
-            if (!force) _gibsCount++;
+            _particle = Object.Instantiate(prefab);
+            _particle.SetActive(false);
+            Pool.Enqueue(_particle);
         }
+    }
+
+    public static void Create(Vector3 position, Particles id, bool force)
+    {
+        if (force || _count < PoolSize * 0.8f)
+        {
+            _particle = GetFromPool();
+            if (_particle)
+            {
+                _particle.SetActive(true);
+                _particle.GetComponent<ParticleComponent>().Spawn(position, id);
+                if (!force) _count++;
+            }
+        }
+    }
+
+    public static GameObject GetFromPool()
+    {
+        if (Pool.Count > 0)
+        {
+            return Pool.Dequeue();
+        }
+        return null;
+    }
+
+    public static void ReturnToPool(GameObject particle)
+    {
+        particle.SetActive(false);
+        Pool.Enqueue(particle);
+        _count--;
     }
 }
