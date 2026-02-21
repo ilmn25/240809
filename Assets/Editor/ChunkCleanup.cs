7a2fc750bc6b78172060c7f34284ee0ff24703a1
illu <kilmn025@gmail.com>
2026-02-22 00:44:04 +0800

    Added Dev QOL, better console etc

---
+++ Assets/Editor/ChunkCleanup.cs
@@ -0,0 +1,37 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+// simple editor helper that purges any leftover "Chunk" objects when the editor
+// loads or exits play mode so they don't stick around in the scene permanently.
+[InitializeOnLoad]
+public static class ChunkCleanup
+{
+    static ChunkCleanup()
+    {
+        // schedule removal when editor is ready
+        EditorApplication.delayCall += RemoveChunks;
+        // also remove when returning from play mode
+        EditorApplication.playModeStateChanged += OnPlayModeChanged;
+    }
+
+    private static void OnPlayModeChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.EnteredEditMode)
+            RemoveChunks();
+    }
+
+    private static void RemoveChunks()
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode)
+            return;
+
+        GameObject[] all = Object.FindObjectsOfType<GameObject>();
+        foreach (var go in all)
+        {
+            if (go.name == "Chunk")
+                Object.DestroyImmediate(go);
+        }
+    }
+}
+#endif
