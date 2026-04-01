0b6b38ec8dbd4d4eb4c3974b6611fcd1c3835ee1
illu <kilmn025@gmail.com>
2026-04-01 18:51:41 +0800

    Adjust MaxFOV value and update storage visibility logic

--- Assets/Script/Game/Brain/Machine/InContainerState.cs
+++ Assets/Script/Game/Brain/Machine/InContainerState.cs
@@ -1,9 +1,11 @@
 public class InContainerState : State
 {
     public Storage Storage;
+    private int _playerIndex = -1;
     public override void OnEnterState()
     {
         Audio.PlaySFX(SfxID.Text);
+        _playerIndex = Control.CurrentPlayerIndex;
         GUIMain.Storage.Storage = Storage;
         GUIMain.RefreshStorage(); 
         GUIMain.Show(true);
@@ -12,6 +14,19 @@ public class InContainerState : State
 
     public override void OnUpdateState()
     {
+        if (_playerIndex != Control.CurrentPlayerIndex)
+        {
+            _playerIndex = Control.CurrentPlayerIndex;
+            if (Storage == Main.PlayerInfo.Storage)
+                GUIMain.Storage.Show(false);
+        }
+
+        if (Main.Player == null)
+        {
+            Machine.SetState<DefaultState>();
+            return;
+        }
+
         if (!GUIMain.Showing || GUIMain.Storage.Storage != Storage ||
             Helper.SquaredDistance(Main.Player.transform.position, Machine.transform.position) > 5*5) { //walk away from npc
             Machine.SetState<DefaultState>();
