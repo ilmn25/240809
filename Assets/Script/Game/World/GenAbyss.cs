70ed4d67cc9910dfb47aa5b1e963754dac3d46e2
illu <kilmn025@gmail.com>
2026-04-11 13:43:43 +0800

    Refactor world generation classes to unify height calculations and improve consistency across terrain types

--- Assets/Script/Game/World/GenAbyss.cs
+++ Assets/Script/Game/World/GenAbyss.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class GenAbyss : Gen
 {
-    public override Vector3Int GetSize() => new Vector3Int(60, 30, 60);
+    public override Vector3Int GetSize() => new Vector3Int(60, 7, 60);
     public override Vector3Int GetSpawnPoint() => new Vector3Int(GetSize().x / 2, GetSize().y - 2, GetSize().z / 2) * World.ChunkSize;
     
     protected override void GenChunk(Vector3Int currentCoordinate, Chunk currentChunk)
